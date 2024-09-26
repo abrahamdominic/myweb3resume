50dc6c9d8c58be1cc92b0af7546242b45062f3e8
dconco <onipededavid208@gmail.com>
2024-09-26 18:25:55 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
     content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
-            height: {
-                'full/5rem': 'calc(100% - 5rem)'
-            },
             fontFamily: {
                 MontserratAlternates: 'Montserrat Alternates',
                 ProtestGuerrilla: 'Protest Guerrilla',
